d06e374f4527bb8883171fd60ff93e6645534bae
vuhe <zhuhe202@qq.com>
2020-11-12 10:29:42 +0800

    feat: 建立主测试函数
    建立主测试函数，放在顶层包目录下

--- src/main/java/top/vuhe/MainApplication.java
+++ src/main/java/top/vuhe/MainApplication.java
@@ -4,4 +4,5 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
 }
---
+++ src/test/java/top/vuhe/MainApplicationTest.java
@@ -0,0 +1,10 @@
+package top.vuhe;
+
+import org.junit.Test;
+
+public class MainApplicationTest {
+    @Test
+    public void mainTest() {
+
+    }
+}
