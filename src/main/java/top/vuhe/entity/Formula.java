6c3a198ed8de6a72a6e571f0de201871c943ae55
vuhe <zhuhe202@qq.com>
2020-11-12 11:21:00 +0800

    feat: 完成基本功能
    算式产生功能；
    题目生成功能；
    加减法计算功能；

---
+++ src/main/java/top/vuhe/entity/Formula.java
@@ -0,0 +1,38 @@
+package top.vuhe.entity;
+
+public class Formula {
+    private final int a;
+    private final OperatorEnum op;
+    private final int b;
+
+    /**
+     * 初始化算式
+     * 为避免不必要的问题，算式在初始化后禁止修改
+     *
+     * @param a  第一个数
+     * @param op 运算符
+     * @param b  第二个数
+     */
+    public Formula(int a, OperatorEnum op, int b) {
+        this.a = a;
+        this.op = op;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public OperatorEnum getOp() {
+        return op;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    @Override
+    public String toString() {
+        return a + " " + op + " " + b + " = ";
+    }
+}
---
+++ src/main/java/top/vuhe/problem/ProblemFactory.java
@@ -0,0 +1,26 @@
+package top.vuhe.problem;
+
+import top.vuhe.entity.Problem;
+import top.vuhe.formula.FormulaFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProblemFactory {
+    public static List<Problem> getTestProblem() {
+        List<Problem> question = new ArrayList<>(51);
+        for (int i = 0; i < 50; i++) {
+            // 用算式方法工厂创建一个算式
+            Problem problem;
+            do {
+                problem = new Problem(FormulaFactory.getFormula());
+                // 此处检查问题的范围是否「超过 100 、小于 0」
+            } while (problem.getAns() > 100 || problem.getAns() < 0);
+
+            // 用刚刚创建的算式，生成一个问题
+            // 加入问题集合
+            question.add(problem);
+        }
+        return question;
+    }
+}
