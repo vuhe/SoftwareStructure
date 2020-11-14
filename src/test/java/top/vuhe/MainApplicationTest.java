5439ccaa1590070fec442a9204cf5bc1ed4a8bff
vuhe <zhuhe202@qq.com>
2020-11-14 15:19:22 +0800

    feat: 完成打包和说明
    对打包部分的进行设置
    同时，添加说明和截图

--- src/test/java/top/vuhe/MainApplicationTest.java
+++ src/test/java/top/vuhe/MainApplicationTest.java
@@ -17,15 +17,15 @@ public class MainApplicationTest {
             }
             System.out.print(problems.get(i).getFormula() + "\t");
         }
-        System.out.println("\n答案：");
-        for (int i = 0; i < 50; i++) {
-            if (i != 0 && i % 5 == 0) {
-                System.out.println();
-            }
-            Problem problem = problems.get(i);
-            System.out.print(problem.getFormula());
-            System.out.print(problem.getAns() + "\t");
-
-        }
+//        System.out.println("\n答案：");
+//        for (int i = 0; i < 50; i++) {
+//            if (i != 0 && i % 5 == 0) {
+//                System.out.println();
+//            }
+//            Problem problem = problems.get(i);
+//            System.out.print(problem.getFormula());
+//            System.out.print(problem.getAns() + "\t");
+//
+//        }
     }
 }
