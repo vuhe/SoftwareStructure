a46ed1ff2082b6fb04ac65b371a79248fb18d58c
vuhe <zhuhe202@qq.com>
2020-11-14 10:39:33 +0800

    feat: 切换至swing组件
    主函数中使用swing测试
    原主函数测试内容移动至test包中

--- src/main/java/top/vuhe/MainApplication.java
+++ src/main/java/top/vuhe/MainApplication.java
@@ -1,30 +1,10 @@
 package top.vuhe;
 
-import top.vuhe.model.Problem;
-import top.vuhe.controller.problem.ProblemFactory;
-
-import java.util.List;
+import top.vuhe.view.MainFrame;
 
 public class MainApplication {
     public static void main(String[] args) {
-        List<Problem> problems = ProblemFactory.getTestProblem();
-        System.out.println("问题：");
-        for (int i = 0; i < 50; i++) {
-            if (i != 0 && i % 5 == 0) {
-                System.out.println();
-            }
-            System.out.print(problems.get(i).getFormula() + "\t");
-        }
-        System.out.println("\n答案：");
-        for (int i = 0; i < 50; i++) {
-            if (i != 0 && i % 5 == 0) {
-                System.out.println();
-            }
-            Problem problem = problems.get(i);
-            System.out.print(problem.getFormula());
-            System.out.print(problem.getAns() + "\t");
-
-        }
+        new MainFrame();
     }
 
 }
---
+++ src/main/java/top/vuhe/view/MainFrame.java
@@ -0,0 +1,12 @@
+package top.vuhe.view;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 100);
+        setVisible(true);
+    }
+}
