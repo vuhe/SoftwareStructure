160fbe54e2e57854a5221eeb93c6c28c3539fdc4
vuhe <zhuhe202@qq.com>
2020-11-15 16:49:38 +0800

    test(test): 添加测试内容
    添加 v0.1 ~ v0.3 的测试内容

--- src/test/java/top/vuhe/MainApplicationTest.java
+++ src/test/java/top/vuhe/MainApplicationTest.java
@@ -1,31 +1,72 @@
 package top.vuhe;
 
 import org.junit.Test;
+import top.vuhe.controller.formula.FormulaFactory;
 import top.vuhe.controller.problem.ProblemFactory;
+import top.vuhe.model.Formula;
 import top.vuhe.model.Problem;
 
 import java.util.List;
 
 public class MainApplicationTest {
+    /**
+     * 版本 v0.1 测试
+     *
+     * 测试算式的产生和运算
+     */
     @Test
-    public void mainTest() {
+    public void testV1() {
         List<Problem> problems = ProblemFactory.getTestProblem();
-        System.out.println("问题：");
+        System.out.println("算式：");
         for (int i = 0; i < 50; i++) {
             if (i != 0 && i % 5 == 0) {
                 System.out.println();
             }
-            System.out.print(problems.get(i).getFormula() + "\t");
+            Problem problem = problems.get(i);
+            Formula formula = problem.getFormula();
+            System.out.printf("%3d", formula.getA());
+            System.out.print(" " + formula.getOp() + " ");
+            System.out.printf("%3d = ", formula.getB());
+            System.out.printf("%3d   ", problem.getAns());
         }
-//        System.out.println("\n答案：");
-//        for (int i = 0; i < 50; i++) {
-//            if (i != 0 && i % 5 == 0) {
-//                System.out.println();
-//            }
-//            Problem problem = problems.get(i);
-//            System.out.print(problem.getFormula());
-//            System.out.print(problem.getAns() + "\t");
-//
-//        }
+    }
+
+    /**
+     * 版本 v0.2 测试
+     *
+     * 测试算式的运算值是否符合 (0, 100)
+     */
+    @Test
+    public void testV2() {
+        // 测试 1000 次
+        for (int i = 0; i < 1000; i++) {
+            Formula formula = FormulaFactory.getFormula();
+            int a = formula.getA();
+            int b = formula.getB();
+            if (a <= 0 || 100 <= a) {
+                throw new RuntimeException("随机数不符合 (0, 100)");
+            }
+            if (b <= 0 || 100 <= b) {
+                throw new RuntimeException("随机数不符合 (0, 100)");
+            }
+        }
+    }
+
+    /**
+     * 版本 v0.3 测试
+     *
+     * 测试算式的运算结果是否符合 [0, 100]
+     */
+    @Test
+    public void testV3() {
+        // 测试 1000 次
+        for (int i = 0; i < 1000; i++) {
+            Formula formula = FormulaFactory.getFormula();
+            Problem problem = new Problem(formula);
+            int ans = problem.getAns();
+            if (ans < 0 || 100 < ans) {
+                throw new RuntimeException("结果不符合 (0, 100)");
+            }
+        }
     }
 }
